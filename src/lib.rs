d1fde35c38c1e73bb7d44303990430b3d90ec39c
Maté Antunovic <mate AT schnooty.com>
2022-03-26 15:58:46 +1300

    Add Transaction and LegacyTransaction trait

--- src/lib.rs
+++ src/lib.rs
@@ -13,43 +13,65 @@ extern crate ethereum_types;
 extern crate serde_json;
 
 use secp256k1::{SecretKey, Message, Secp256k1};
+use tiny_keccak::{Keccak, Hasher};
 use rlp::RlpStream;
-use rlp::Encodable;
 
-mod raw_transaction;
-pub use self::raw_transaction::RawTransaction;
+//mod raw_transaction;
+//pub use self::raw_transaction::RawTransaction;
 
 pub trait Transaction {
-    fn chain_id(&self) -> u64;
-    fn rlp(&self) -> &[&dyn Encodable];
+    fn chain(&self) -> u64;
+    fn hash(&self) -> [u8; 32];
+    fn encode(&self, private_key: &[u8]) -> Vec<u8>;
+    fn ecdsa(&self, private_key: &[u8]) -> EcdsaSig;
+}
 
-    fn hash(&self) -> Vec<u8> {
-        let mut hash = RlpStream::new();
-        hash.begin_unbounded_list();
-        for e in self.rlp().iter() {
-            hash.endcode(e);
-        }
-        hash.append(&chain_id.clone());
+/// Description of a Transaction, pending or in the chain.
+#[derive(Debug, Default, Clone, PartialEq, Deserialize, Serialize)]
+pub struct LegacyTransaction {
+    /// Chain ID
+    pub chain: u64,
+    /// Nonce
+    pub nonce: u128,
+    /// Recipient (None when contract creation)
+    pub to: Option<[u8; 20]>,
+    /// Transfered value
+    pub value: u128,
+    /// Gas Price
+    #[serde(rename = "gasPrice")]
+    pub gas_price: u128,
+    /// Gas amount
+    pub gas: u128,
+    /// Input data
+    pub data: Vec<u8>,
+}
+
+impl LegacyTransaction {
+    fn rlp(&self) -> RlpStream {
+        unimplemented!()
+    }
+}
+
+impl Transaction for LegacyTransaction {
+    fn chain(&self) -> u64 {
+        self.chain
+    }
+
+    fn hash(&self) -> [u8; 32] {
+        let mut hash = self.rlp();
+        //hash.begin_unbounded_list();
+        //for e in self.rlp().iter() {
+        //    hash.append(e);
+        //}
+        hash.append(&self.chain());
         hash.append_raw(&[0x80], 1);
         hash.append_raw(&[0x80], 1);
         hash.finalize_unbounded_list();
         keccak256_hash(&hash.out())
     }
 
-    fn ecdsa(&self, private_key: &[u8]) -> EcdsaSig {
-        let chain_id = self.chain_id();
-        let hash = self.hash();
-
-        EcdsaSig::generate(&hash, private_key, &chain_id_u64)
-    }
-
-    fn sign(&self, private_key: &[u8]) -> Result<Vec<u8>, SigError> {
-        let mut rlp_stream = RlpStream::new();
-        rlp_stream.begin_unbounded_list();
-
-        for e in self.rlp().iter() {
-            e.rlp_append(&mut rlp_stream);
-        }
+    fn encode(&self, private_key: &[u8]) -> Vec<u8> {
+        let mut rlp_stream = self.rlp();
 
         match self.ecdsa(private_key) {
             EcdsaSig { v, s, r} => {
@@ -61,17 +83,14 @@ pub trait Transaction {
 
         rlp_stream.finalize_unbounded_list();
 
-        return Ok(rlp_stream.out().to_vec())
+        return rlp_stream.out().to_vec()
     }
-}
 
-pub trait TypedTransaction {
-    fn transaction_type(&self) -> u8;
-}
+    fn ecdsa(&self, private_key: &[u8]) -> EcdsaSig {
+        let hash = self.hash();
 
-pub enum SigError {
-    /// If rlp().is_finished() is true, sign() will return this error.
-    RlpStreamEnd
+        EcdsaSig::generate(hash, private_key, self.chain())
+    }
 }
 
 pub struct EcdsaSig {
@@ -81,9 +100,9 @@ pub struct EcdsaSig {
 }
 
 impl EcdsaSig {
-    pub fn generate(hash: &[u8], private_key: &[u8], chain_id: &u64) -> EcdsaSig {
+    pub fn generate(hash: [u8; 32], private_key: &[u8], chain_id: u64) -> EcdsaSig {
         let s = Secp256k1::signing_only();
-        let msg = Message::from_slice(hash).unwrap();
+        let msg = Message::from_slice(&hash).unwrap();
         let key = SecretKey::from_slice(private_key).unwrap();
         let (v, sig_bytes) = s.sign_ecdsa_recoverable(&msg, &key).serialize_compact();
 
@@ -95,12 +114,10 @@ impl EcdsaSig {
     }
 }
 
-pub fn keccak256_hash(bytes: &[u8]) -> Vec<u8> {
+pub fn keccak256_hash(bytes: &[u8]) -> [u8; 32] {
     let mut hasher = Keccak::v256();
     hasher.update(bytes);
     let mut resp: [u8; 32] = Default::default();
     hasher.finalize(&mut resp);
-    resp.iter().cloned().collect()
+    resp
 }
-
-
